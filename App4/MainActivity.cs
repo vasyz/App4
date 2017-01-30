0d5d0b18b2dbde542fe384e1253540ed3426b7ec
Inredika WhiteBlack <InRedikaWB@users.noreply.github.com>
2017-01-30 20:09:10 +0200

    v0.0.4

--- App4/MainActivity.cs
+++ App4/MainActivity.cs
@@ -12,39 +12,60 @@ using Microsoft.CSharp;
 
 using ChatProject.Tools;
 using System.Collections.Generic;
+using Android.Locations;
+using Android.Runtime;
+using System;
+using Android.Webkit;
+
 
 namespace ChatProject
 {
-    [Activity(Label = "App4", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Chat Message", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, ILocationListener
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
-            Button btn = FindViewById<Button>(Resource.Id.button1);
-            TextView tw = FindViewById<TextView>(Resource.Id.textView1);
-            EditText inputMethod = FindViewById<EditText>(Resource.Id.editText1);
-            tw.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
+            LocationManager locmanager = GetSystemService(Context.LocationService) as LocationManager;
+            Button btnRefresh = FindViewById<Button>(Resource.Id.btnRefresh);
+            Button btnSend = FindViewById<Button>(Resource.Id.btnSend);
+            TextView twChatHistory = FindViewById<TextView>(Resource.Id.chatHistory);
+            EditText inputMethod = FindViewById<EditText>(Resource.Id.inputMessage);
+            twChatHistory.MovementMethod = new Android.Text.Method.ScrollingMovementMethod();
 
-            btn.Click += (sender, e) =>
+            btnSend.Click += delegate
             {
                 ChatSend.GetChatSendin(inputMethod.Text);
-                List<ChatMessage> chatHistory = Tools.ChatRequest.GetChatMessage();
-                tw.Text = "";
-                foreach (var msg in chatHistory)
-                {
-                    tw.Text += msg.Author + ": " + msg.Text + "\n";
-                }
-
-
-
+                inputMethod.Text = "";
             };
 
-        
-                
+            btnRefresh.Click += delegate
+            {
+                locmanager.RequestLocationUpdates(LocationManager.NetworkProvider, 2000, 1, this);
+                List<ChatMessage> chatHistory = Tools.ChatRequest.GetChatMessage();
+                twChatHistory.Text = "";
+                foreach (var msg in chatHistory)
+                {
+                    twChatHistory.Text += msg.Author + ": " + msg.Text + "\n";
+                }
+            };
         }
 
+        public void OnLocationChanged(Location location)
+        {
+            Button btnUpdate = FindViewById<Button>(Resource.Id.btnRefresh);
+            btnUpdate.Text = location.Longitude.ToString() + " | " + location.Latitude.ToString();
+        }
+        public void OnProviderDisabled(string provider)
+        {
+        }
+        public void OnProviderEnabled(string provider)
+        {
+        }
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+        }
     }
 }
 
